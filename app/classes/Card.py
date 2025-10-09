fa8c7e60da8f66d3ff36802012c220d7481f5c84
Diego <diego.desdemallorca@gmail.com>
2025-10-09 18:27:43 +0200

    Second_exercice

--- app/classes/Card.py
+++ app/classes/Card.py
@@ -7,11 +7,28 @@ class Card:
         self.id = id
         self.name = name
 
-
     def __str__(self):
         return f"{self.name, self.id}"
 
 
+class PokemonCard(Card):
+    def __init__(self, id, name):
+        super().__init__(id, name)
 
-card = Card(id=4, name="Pikatchu")
-print(card)
+
+class EnergyCard(Card):
+    def __init__(self, id, name):
+        super().__init__(id, name)
+
+
+class TrainerCard(Card):
+    def __init__(self, id, name):
+        super().__init__(id, name)
+
+
+pokemonCard = PokemonCard(id=5, name="Pikachu")
+energyCard = EnergyCard(id=6, name="Fire")
+trainerCard = TrainerCard(id=7, name="Potion")
+print(pokemonCard)
+print(energyCard)
+print(trainerCard)
