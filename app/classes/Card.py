f954e01bd2115cc350fee6da484b5610ffa4f8f0
Diego9375 <diego.desdemallorca@gmail.com>
2025-10-23 15:14:01 +0200

    Fifth exercice (#4)
    * Second_exercice
    
    * third_exercise
    
    * fourth_exercice
    
    * Addedd validation for base damage
    
    * added exercice
    
    ---------
    
    Co-authored-by: iCabellos <ivancabe.96@gmail.com>

--- app/classes/Card.py
+++ app/classes/Card.py
@@ -13,7 +13,6 @@ class Type(Enum):
     FIGHTING = 6
     COLOURLESS = 7
 
-
 class Attack:
     def __init__(self, id, name, base_damage):
         self.id = id
@@ -46,24 +45,32 @@ class PokemonCard(Card):
         self.hp = hp
         self.hp_current = self.hp
         self.attacks = attacks
+        self.attached_energy: dict = {}
 
     def __str__(self):
         return f"{self.name, self.id, self.type, self.hp}"
 
 
 class EnergyCard(Card):
-    def __init__(self, id, name):
+    def __init__(self, id, name, etype):
         super().__init__(id, name)
+        self.etype:Type = etype
 
 
 class TrainerCard(Card):
     def __init__(self, id, name):
         super().__init__(id, name)
 
+def attach_energy(pokemon: PokemonCard, energy_card: EnergyCard):
+    if energy_card.etype not in pokemon.attached_energy:
+        pokemon.attached_energy[energy_card.etype] = 0
+    pokemon.attached_energy[energy_card.etype] += 1
+    print(pokemon.attached_energy)
 
-rayo = Attack(id=8, name="Rayo", base_damage=-56)
+
+rayo = Attack(id=8, name="Rayo", base_damage=100)
 pokemonCard = PokemonCard(id=5, name="Pikachu", type=Type.LIGHTNING, hp=100, attacks=[rayo])
-energyCard = EnergyCard(id=6, name="Fire")
+energyCard = EnergyCard(id=6, name="Colourless", etype=Type.COLOURLESS)
 trainerCard = TrainerCard(id=7, name="Potion")
 print(pokemonCard)
 print(energyCard)
